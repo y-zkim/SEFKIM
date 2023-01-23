9a219af8a6c69a6aa127f693211e8de71638f95d
y-zkim <youssef.zkim1998@gmail.com>
2023-01-23 13:13:10 +0100

    added notification push method in main activity

--- app/src/main/java/com/insacvl/sefkim_flickr/MainActivity.java
+++ app/src/main/java/com/insacvl/sefkim_flickr/MainActivity.java
@@ -3,9 +3,19 @@ package com.insacvl.sefkim_flickr;
 * @Author : ZKIM Youssef
 */
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +23,7 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.NotificationCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -43,6 +54,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
  */
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NOTIF_ID = 123;
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -85,12 +97,48 @@ public class MainActivity extends AppCompatActivity {
                 navController.navigate(R.id.settingsFragment);
             }
         });
+        // notification will go here
+//        int reqCode = 1;
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        showNotification(getApplicationContext(), "SEFKIM", "Welcome to SIFKIM", intent, reqCode);
     }
 
+
+//  To push notifications on app startup
+    public void showNotification(Context context, String title, String message, Intent intent, int reqCode) {
+//        SharedPreferenceManager sharedPreferenceManager = SharedPreferenceManager.getInstance(context);
+
+        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getActivity(context, reqCode, intent, PendingIntent.FLAG_ONE_SHOT);
+        System.out.println("pendingIntent = " + pendingIntent);
+        String CHANNEL_ID = "channel_id";// The id of the channel.
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.app_logo)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(pendingIntent);
+        System.out.println("notificationBuilder = " + notificationBuilder);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        System.out.println("notificationManager = " + notificationManager);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            System.out.println(" inside if ");
+            CharSequence name = "Channel Name";// The user-visible name of the channel.
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            notificationManager.createNotificationChannel(mChannel);
+            System.out.println("mChannel = " + mChannel);
+        }
+        notificationManager.notify(reqCode, notificationBuilder.build()); // 0 is the request code, it should be unique id
+        System.out.println("notificationManager notifying= " + notificationManager);
+
+        Log.d("showNotification", "showNotification: " + reqCode);
+    }
     /**
      * This method handles the navigation up event.
      * @return true if the event is handled, false otherwise
      */
+
     @Override
     public boolean onSupportNavigateUp() {
 //        return super.onSupportNavigateUp();
@@ -98,4 +146,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
